c624c3140b75d34f1f52026a5a148f7d41b69254
joaovian06 <joaovitor_vian@hotmail.com>
2024-01-08 00:31:54 -0300

    article unit testing

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,17 @@
 class Article < ApplicationRecord
+    # Validations
+    validates :title, 
+        presence: true, 
+        length: { in: 10..50 }, 
+        uniqueness: { 
+            scope: :author, 
+            message: "Title already taken, please choose another." 
+        }
+    validates :body, presence: true
+    validates :author,
+        presence: true,
+        format: {
+            without: /\A[0-9]*\z/,
+            message: "Author can not have only numbers."
+        }
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "factory" do
+    context "valid" do
+      let(:article) { create(:article) }
+
+      it { expect(article).to be_valid }
+    end
+
+    context "traits" do
+      context "invalid" do
+        let(:article) { create(:article, :invalid) }
+
+        it { is_expected.to be_invalid }
+      end
+    end
+  end
+
+  describe "columns" do
+    it { is_expected.to have_db_column(:title) }
+    it { is_expected.to have_db_column(:body) }
+    it { is_expected.to have_db_column(:author) }
+  end
+
+  describe "validations" do
+    context "title" do
+      it { is_expected.to validate_presence_of(:title) }
+      it do
+        is_expected.to validate_length_of(:title)
+          .is_at_least(10)
+          .is_at_most(50)
+      end
+
+      it do
+        is_expected.to validate_uniqueness_of(:title)
+          .scoped_to(:author)
+          .with_message("Title already taken, please choose another.")
+      end
+    end
+
+    context "body" do
+      it { is_expected.to validate_presence_of(:body) }
+    end
+
+
+    context "author" do
+      # let(:article_invalid_author) { build(:article, author: "1234") }
+
+      it { is_expected.to validate_presence_of(:author) }
+      it do
+        # expect(article_invalid_author.validate).to be_falsy 
+        is_expected.not_to allow_value("1234")
+          .for(:author)
+          .with_message("Author can not have only numbers.")
+      end
+    end
+  end
 end
